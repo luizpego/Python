1ba666d3130b81bd6ea0766bb4a0f7d898e93921
Luiz Andre Fernandes Pego <luizandreafonsopego@gmail.com>
2026-08-18 13:24:11 -0300

    .

--- Django/PROJETO_CURSOS/MEU_PROJETO/cursos/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Professor, Curso, Aluno
-# Register your models here.
-
-admin.site.register(Curso)
-admin.site.register(Professor)
-admin.site.register(Aluno)
